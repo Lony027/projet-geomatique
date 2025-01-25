24d2379fbacaf16d75235cae49cdbaf2db64c872
Lony027 <iony@protonmail.com>
2025-01-26 00:23:37 +0100

    hihi jveux canner

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// FIX ZOOM!
-// @ to-do: features gagnant (: front et partie dynamique, affichage intuitif, // opacité max sur 1 et min sur 0.10)
-// css menu. prends pas le max de width
-// @ to-do: check css et html
+// @ to-do: fix darken
 // cannot hover with label
+// fix zoom
 
-// drom bug
+// github ownership
 // add more years
 
 // Tout mettre en anglais
 // 1 seul .js
-// 1 seul CSS
 // rapport
 // Readme
+// TEEST
 // @ to-do: last clean
+// TEEST
 
 // -----------------------
 
@@ -20,7 +19,25 @@ let lastRegionLayer;
 let lastDepLayer;
 
 let presidentialData;
-let partysData, regionDataGeoJson, departmentDataGeoJson, circoDataGeoJson;
+let partysData,
+  regionDataGeoJson,
+  departmentDataGeoJson,
+  circoDataGeoJson,
+  depByRegionData;
+
+const availableYear = [
+  '2012',
+  '2007',
+  '2002',
+  '1995',
+  '1988',
+  '1981',
+  '1974',
+  '1969',
+  '1965',
+];
+
+const availableTour = ['1', '2'];
 const regionsList = [
   '11',
   '24',
@@ -41,14 +58,30 @@ const regionsList = [
   '04',
   '06',
 ];
+const drom = ['01', '02', '03', '04'];
 async function initData() {
   try {
+    // let regionPromises = [];
+    // regionsList.forEach((regionCode) =>
+    //   regionPromises.push(fetchData(`https://geo.api.gouv.fr/regions/${regionCode}/departements`))
+    // );
+
+    let regionPromises = regionsList.map((regionCode) =>
+      fetchData(
+        `https://geo.api.gouv.fr/regions/${regionCode}/departements`
+      ).then((departments) => ({
+        regionCode,
+        departments,
+      }))
+    );
+
     const promise = await Promise.all([
       fetchData('ressources/parties.json'),
       fetchData('ressources/geojson/regions.geojson'),
       fetchData('ressources/geojson/departements.geojson'),
       fetchData('ressources/geojson/circonscriptions.geojson'),
-      // fetchData('https://geo.api.gouv.fr/regions'),
+      // ...regionPromises,
+      ...regionPromises.map((p) => p.then((data) => data.departments)),
       setDataPresidentielles(),
     ]);
 
@@ -56,7 +89,15 @@ async function initData() {
     regionDataGeoJson = promise[1];
     departmentDataGeoJson = promise[2];
     circoDataGeoJson = promise[3];
-    // regionsList = promise[4];
+    // depByRegionData = promise.slice(4, 4 + regionsList.length).flat();
+
+    depByRegionData = {};
+    const regionResults = await Promise.all(regionPromises);
+    regionResults.forEach(({ regionCode, departments }) => {
+      depByRegionData[regionCode] = departments;
+    });
+
+    console.log(depByRegionData);
   } catch (e) {
     throw new Error('Error while fetching data');
   }
@@ -129,23 +170,21 @@ function definePane() {
   );
 }
 
-let breadcrumbElement, breadcrumb, isWinning, selectedParty;
+let breadcrumbElement, breadcrumb, isWinning;
 const selectYear = document.getElementById('selectYear');
 const btnWinning = document.getElementById('btn_winning');
-const btnCandidat = document.getElementById('btn_candidat');
-const selectedTurn = document.getElementById('tour');
+const btnCandidat = document.getElementById('btn_candidate');
+const selectedTurn = document.getElementById('turn');
+const candidates = document.getElementById('slct_candidates');
 function getHTMLElements() {
   breadcrumbElement = document.getElementById('breadcrumb');
   breadcrumb = [];
-
-  // smarter way to do
-
-  selectedParty = 'SARKOZY (UMP)';
 
   isWinning = 1;
   btnWinning.addEventListener('click', function () {
     btnWinning.classList.add('selected');
     btnCandidat.classList.remove('selected');
+    candidates.style.visibility = 'hidden';
     isWinning = 1;
     reloadLayer();
   });
@@ -153,6 +192,7 @@ function getHTMLElements() {
   btnCandidat.addEventListener('click', function () {
     btnCandidat.classList.add('selected');
     btnWinning.classList.remove('selected');
+    candidates.style.visibility = 'visible';
     isWinning = 0;
     reloadLayer();
   });
@@ -165,15 +205,36 @@ function getHTMLElements() {
   });
 
   selectYear.addEventListener('change', reloadLayer);
-
-
   selectedTurn.addEventListener('change', reloadLayer);
+  candidates.addEventListener('change', reloadLayer);
 }
 
 let currentPartys;
 function init() {
   currentPartys = getParty(presidentialData);
+  // selectedParty = currentPartys[0];
+  fillCandidates();
+  selectedPartyListener();
   generateLayerByGeoJson();
+}
+
+function fillCandidates() {
+  if (isWinning != 0) {
+    return;
+  }
+  const currentSelection = candidates.value;
+  candidates.innerHTML = '';
+  currentPartys.forEach((party) => {
+    const option = document.createElement('option');
+    option.value = party;
+    option.textContent = party;
+    candidates.appendChild(option);
+  });
+  if (currentPartys.includes(currentSelection)) {
+    candidates.value = currentSelection;
+  } else {
+    selectedParty = currentPartys[0];
+  }
 }
 
 window.onload = async () => {
@@ -194,10 +255,26 @@ function replaceAndTruncateArray(array, index, newValue) {
   return array.slice(0, index).concat(newValue);
 }
 
+// function removeEachLayer(zoneGroup) {
+//   zoneGroup.eachLayer((layer) => {
+//     map.removeLayer(layer);
+//   });
+// }
+
+// function addEachLayer(zoneGroup) {
+//   zoneGroup.eachLayer((layer) => {
+//     console.log(layer);
+//     map.addLayer(layer);
+//   });
+// }
+
 function dynamicClearLayers(level) {
   regionLabelLayerGroup.clearLayers();
   departmentLabelLayerGroup.clearLayers();
   circoLabelLayerGroup.clearLayers();
+  // removeEachLayer(regionLabelLayerGroup);
+  // removeEachLayer(departmentLabelLayerGroup);
+  // removeEachLayer(circoLabelLayerGroup);
   switch (level) {
     case 'region':
       departementsLayerGroup.clearLayers();
@@ -295,21 +372,26 @@ function breadcrumbClick(depth) {
 
 function getSelectedYear() {
   return (
-    selectYear.options[selectYear.selectedIndex]?.value ||
-    availableYear[0]
+    selectYear.options[selectYear.selectedIndex]?.value || availableYear[0]
   );
 }
 
 function getSelectedTour() {
-  return (
-    selectedTurn.options[selectedTurn.selectedIndex]?.value || '1'
-  );
+  return selectedTurn.options[selectedTurn.selectedIndex]?.value || '1';
 }
 
 async function setDataPresidentielles() {
   const selectedYear = getSelectedYear();
   const selectedTour = getSelectedTour();
   presidentialData = await loadDataPresidentielles(selectedYear, selectedTour);
+}
+
+function selectedPartyListener() {
+  candidates.addEventListener('click', function () {
+    if (isWinning == 0) {
+      selectedParty = candidates.options[candidates.selectedIndex]?.value;
+    }
+  });
 }
 
 // -------------------------- LAYER MANAGEMENT
@@ -332,10 +414,11 @@ async function reloadLayer() {
   //   console.log(await getPartyScoreForAllRegions(selectedParty));
   //   // minPartySelected()
   // }
-
+  fillTitle('France');
   map.setView([47.0811658, 2.399125], 6);
   await setDataPresidentielles();
   currentPartys = getParty(presidentialData);
+  fillCandidates();
   generateLayerByGeoJson();
 }
 
@@ -547,10 +630,57 @@ function showLabelOnZoom() {
   });
 }
 
+// Function From https://interactively.info/
+function darkenColor(color, percent) {
+  // Strip the leading # if it's there
+  color = color.replace(/^\s*#|\s*$/g, '');
+
+  // Convert 3 char codes -> 6, e.g. `E0F` -> `EE00FF`
+  if (color.length == 3) {
+    color = color.replace(/(.)/g, '$1$1');
+  }
+
+  // Split HEX Color
+  const hexR = color.substring(0, 2);
+  const hexG = color.substring(2, 4);
+  const hexB = color.substring(4, 6);
+
+  // HEX to RGB
+  let r = parseInt(hexR, 16);
+  let g = parseInt(hexG, 16);
+  let b = parseInt(hexB, 16);
+
+  if (isNaN(r)) r = 0;
+  if (isNaN(g)) g = 0;
+  if (isNaN(b)) b = 0;
+
+  // Manipulate
+  // percent = 1 + (percent / 100);
+  const newR = Math.min(255, Math.floor(r + (r * percent) / 100));
+  const newG = Math.min(255, Math.floor(g + (g * percent) / 100));
+  const newB = Math.min(255, Math.floor(b + (b * percent) / 100));
+
+  // RGB to HEX
+  const newHexRColor = `${newR.toString(16)}`.padStart(2, '0');
+  const newHexGColor = `${newG.toString(16)}`.padStart(2, '0');
+  const newHexBColor = `${newB.toString(16)}`.padStart(2, '0');
+
+  return '#' + newHexRColor + newHexGColor + newHexBColor;
+}
+
 function colorByPercent(layer, color, percent) {
+  if (percent < 5) {
+    percent = percent * 50;
+  } else if (percent < 20) {
+    percent = percent * 5;
+  } else if (percent < 35) {
+    percent = percent * 2;
+  }
+
+  color = darkenColor(color, -percent);
   layer.setStyle({
     color: '#F1F1F1',
-    fillOpacity: percent,
+    fillOpacity: 0.6,
     fillColor: color,
   });
 }
@@ -564,7 +694,7 @@ function eachFeatureWrapper(feature, layer, data, depth) {
   } else {
     let color = colorByParty(selectedParty);
     let percent = getPercentByParty(data, selectedParty);
-    colorByPercent(layer, color, percent / 100);
+    colorByPercent(layer, color, percent);
   }
   setWeighByDepth(layer, depth);
   layerHover(layer, depth);
@@ -573,12 +703,25 @@ function eachFeatureWrapper(feature, layer, data, depth) {
 
 // -------------------------- MOVE TO LAYER
 
+function restoreLastZone(lastZone) {
+  if (lastZone) {
+    if (isWinning) {
+      lastZone.setStyle({ opacity: 1, fillOpacity: 0.2 });
+    } else {
+      lastZone.setStyle({ opacity: 1, fillOpacity: 0.6 });
+    }
+  }
+}
 function generateLayerByGeoJson() {
   fillTableWithData(getFranceResults(presidentialData));
   let geoJsonLayer = L.geoJSON(regionDataGeoJson, {
     onEachFeature: async function (feature, layer) {
       const regionCode = feature.properties.code;
-      const regionData = await getByRegion(presidentialData, regionCode);
+      const regionData = await getByRegion(
+        presidentialData,
+        regionCode,
+        depByRegionData[regionCode]
+      );
       if (!regionData) {
         return;
       }
@@ -589,21 +732,24 @@ function generateLayerByGeoJson() {
 }
 
 async function moveToRegion(regionLayer) {
-  if (lastRegionLayer) {
-    lastRegionLayer.setStyle({ opacity: 1, fillOpacity: 0.2 });
-  }
-  dynamicClearLayers('region');
   map.fitBounds(regionLayer.getBounds());
-
   const regionCode = regionLayer.feature.properties.code;
-  const departements = await fetchData(
-    `https://geo.api.gouv.fr/regions/${regionCode}/departements`
-  );
+  if (drom.includes(regionCode)) {
+    return;
+  }
+  restoreLastZone(lastRegionLayer);
+  dynamicClearLayers('region');
+
+  const departements = depByRegionData[regionCode];
+
+  // console.log(regionCode);
+  // 01, 02, 03, 04
 
   const filteredDepartements = departmentDataGeoJson.features.filter(
     (feature) =>
       departements.some((dep) => dep.code === feature.properties.code)
   );
+
   if (!filteredDepartements) {
     return;
   }
@@ -624,13 +770,12 @@ async function moveToRegion(regionLayer) {
   departementLayer.addTo(departementsLayerGroup);
 
   lastRegionLayer = regionLayer;
-  regionLayer.setStyle({ opacity: 0, fillOpacity: 0 });
+  lastRegionLayer.setStyle({ opacity: 0, fillOpacity: 0 });
 }
 
 async function moveToDepartments(departmentLayer) {
-  if (lastDepLayer) {
-    lastDepLayer.setStyle({ opacity: 1, fillOpacity: 0.2 });
-  }
+  restoreLastZone(lastDepLayer);
+
   dynamicClearLayers('departement');
   map.fitBounds(departmentLayer.getBounds());
 
@@ -659,7 +804,7 @@ async function moveToDepartments(departmentLayer) {
   circoLayer.addTo(circoLayerGroup);
 
   lastDepLayer = departmentLayer;
-  departmentLayer.setStyle({ opacity: 0, fillOpacity: 0 });
+  lastDepLayer.setStyle({ opacity: 0, fillOpacity: 0 });
 }
 
 async function moveToCirco(circoLayer) {
@@ -681,19 +826,28 @@ function layerByDepthName(depthLayer, depthName) {
 
 function zoomToFrance() {
   dynamicClearLayers('region');
+  // addEachLayer(regionLabelLayerGroup);
+  fillTitle('France');
+  restoreLastZone(lastRegionLayer);
   map.setView([47.0811658, 2.399125], 6);
 }
 
 function zoomToRegion(regionName) {
   dynamicClearLayers('departement');
+  fillTitle(regionName);
+  // addEachLayer(departmentLabelLayerGroup);
+  map.addLayer(departementsLayerGroup);
   let regionLayer = layerByDepthName(regionDataGeoJson, regionName);
   if (!regionLayer) {
     return;
   }
+  restoreLastZone(lastDepLayer);
   map.fitBounds(regionLayer.getBounds());
 }
 
 function zoomToDepartement(departementName) {
+  fillTitle(departementName);
+  // addEachLayer(circoLabelLayerGroup);
   let departmentLayer = layerByDepthName(
     departmentDataGeoJson,
     departementName
@@ -716,3 +870,114 @@ function colorByParty(partyName) {
 //   });
 //   return jsonToReturn;
 // }
+
+function getByDepartmentCode(data, departementCode) {
+  const circos = data.filter((v) => v.code_departement == departementCode);
+  return jsonReduceHelper(data, circos, departementCode, 'code_departement');
+}
+
+function getByDepartmentCodes(data, departementCodeArray) {
+  return data.filter((v) => departementCodeArray.includes(v.code_departement));
+}
+
+function getByCirconscription(data, departementCode, circonscription) {
+  return data.filter(
+    (v) =>
+      v.circonscription == circonscription &&
+      v.code_departement == departementCode
+  );
+}
+
+async function getByRegion(data, codeRegion, departements) {
+  // let departements = await fetchData(
+  //   `https://geo.api.gouv.fr/regions/${codeRegion}/departements`
+  // );
+  // if (departements == undefined) {
+  //   return;
+  // }
+
+  dep_code_array = departements.map((dep) => dep.code);
+  return getRegionByDepartmentArray(data, dep_code_array, codeRegion);
+}
+
+// -----------------------------------------------------------------------------------
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function loadDataPresidentielles(year, tour) {
+  if (!availableYear.includes(year) || !availableTour) {
+    throw new Error(`year : ${year} or turn : ${tour} does not exist`);
+  }
+  let file_name = `presi${year}t${tour}.json`;
+  let file = await fetchData('/ressources/presidentielles/' + file_name);
+  return file;
+}
+
+function getRegionByDepartmentArray(data, dep_code_array, codeRegion) {
+  const departements = getByDepartmentCodes(data, dep_code_array);
+  if (departements.length == 0) {
+    return;
+  }
+  return jsonReduceHelper(data, departements, codeRegion, 'code_region');
+}
+
+function getFranceResults(data) {
+  return jsonReduceHelper(data, data, '', '');
+}
+
+function getParty(data) {
+  let keys = Object.keys(data[0]);
+  return keys.filter((v) => v.match(/^.*\)$/g));
+}
+
+// async function getPartyScoreForAllRegions(party) {
+//   let val = [];
+//   let reg;
+//   // https://geo.api.gouv.fr/departements
+
+//   await regionsList.forEach(r => {
+//     reg = getByRegion(presidentialData, r);
+//     val.push(reg[party]);
+//   })
+//   return val;
+// }
+
+function jsonReduceHelper(data, array, code, keyName) {
+  const parties = getParty(data);
+  let JSONtoReturn = array.reduce(
+    (acc, dep) => {
+      acc[keyName] = code;
+      acc.inscrits += dep.inscrits;
+      acc.blancs_et_nuls += dep.blancs_et_nuls;
+      acc.exprimes += dep.exprimes;
+
+      parties.forEach((partie) => {
+        acc[partie] = (acc[partie] || 0) + (dep[partie] || 0);
+      });
+
+      return acc;
+    },
+    {
+      [keyName]: code,
+      inscrits: 0,
+      exprimes: 0,
+      blancs_et_nuls: 0,
+
+      ...Object.fromEntries(parties.map((party) => [party, 0])),
+    }
+  );
+  return JSONtoReturn;
+}
+
+// -----------------------------------------------------------------------------------
